4ea9e946a051b1657017a2f2dbb911dbb3c61044
RishushSliit <153417080+RishushSliit@users.noreply.github.com>
2024-04-26 09:16:40 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,16 @@
 #include <stdio.h>
 
 int main() {
-  
+   
+int num1,num2;
+prinf("enter fist sub mark : ");
+scanf("%d",num1);   
+prinf("enter second sub mark : ");
+scanf("%d",num2);
+
+num1=(num1+num2)/2;
+
+   prinf("average of the two marks :%d ",num1);
   return 0;
 }
 
