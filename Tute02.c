2bd21b71559848266bd067ae4d5d9a80eb02cd48
RishushSliit <153417080+RishushSliit@users.noreply.github.com>
2024-04-26 09:22:31 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,20 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  int km;
+  float total=0;
+
+prinf("enter  distance the van has travelled : ");
+scanf("%d",km);
+
+if(km<=30){
+  total=50*km;
+}
+else if(km>30){
+  total=30*50+(km-30)*40;
+}
+  
+prinf("Amount :%.2f ",total);
   
   return 0;
 }
