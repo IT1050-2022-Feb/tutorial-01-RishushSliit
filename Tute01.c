d30f311120eabb3f271804cdd0dd5f08f56f231a
RishushSliit <153417080+RishushSliit@users.noreply.github.com>
2024-04-26 09:28:06 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -8,9 +8,9 @@ int main() {
    
 int num1,num2;
 prinf("enter fist sub mark : ");
-scanf("%d",num1);   
+scanf("%d",&num1);   
 prinf("enter second sub mark : ");
-scanf("%d",num2);
+scanf("%d",&num2);
 
 num1=(num1+num2)/2;
 
